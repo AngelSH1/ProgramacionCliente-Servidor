5baaaca0882fb5fcf8ee5bde732b59398c979c64
lney24 <mendezdabney@hotmail.es>
2023-08-14 21:03:19 -0600

    Agregados los datos a la tabla rutina de clientes

--- ProyectoFinal/src/main/java/com/mycompany/proyectofinal/InterfazCliente/PanelRutinaCliente.java
+++ ProyectoFinal/src/main/java/com/mycompany/proyectofinal/InterfazCliente/PanelRutinaCliente.java
@@ -1,6 +1,7 @@
 package com.mycompany.proyectofinal.InterfazCliente;
 
 import com.mycompany.proyectofinal.InfoClase;
+import com.mycompany.proyectofinal.InterfazEmpleado.PanelRutinaEmpleado;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -13,6 +14,7 @@ public class PanelRutinaCliente extends javax.swing.JPanel {
 
     public PanelRutinaCliente() {
         initComponents();
+        llenarTabla();
     }
 
     
@@ -22,8 +24,6 @@ public class PanelRutinaCliente extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnselecionar = new javax.swing.JButton();
-        btnRegistrarRutina = new javax.swing.JButton();
         lbId = new javax.swing.JLabel();
         TablaInstructores = new javax.swing.JScrollPane();
         Rutinas = new javax.swing.JTable();
@@ -32,26 +32,6 @@ public class PanelRutinaCliente extends javax.swing.JPanel {
         setMinimumSize(new java.awt.Dimension(990, 460));
         setPreferredSize(new java.awt.Dimension(990, 460));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        btnselecionar.setBackground(new java.awt.Color(255, 153, 51));
-        btnselecionar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/imagenes/Boton Seleccionar.png"))); // NOI18N
-        btnselecionar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnselecionarActionPerformed(evt);
-            }
-        });
-        add(btnselecionar, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 370, 120, 20));
-
-        btnRegistrarRutina.setBackground(new java.awt.Color(246, 104, 68));
-        btnRegistrarRutina.setFont(new java.awt.Font("Eras Demi ITC", 0, 18)); // NOI18N
-        btnRegistrarRutina.setForeground(new java.awt.Color(255, 255, 255));
-        btnRegistrarRutina.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/imagenes/Boton Registrar_.png"))); // NOI18N
-        btnRegistrarRutina.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRegistrarRutinaActionPerformed(evt);
-            }
-        });
-        add(btnRegistrarRutina, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 400, 150, 40));
 
         lbId.setText("0");
         add(lbId, new org.netbeans.lib.awtextra.AbsoluteConstraints(960, 440, 30, -1));
@@ -75,31 +55,11 @@ public class PanelRutinaCliente extends javax.swing.JPanel {
         });
         TablaInstructores.setViewportView(Rutinas);
 
-        add(TablaInstructores, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 810, 430));
+        add(TablaInstructores, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 970, 430));
 
         Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/imagenes/Fondo.png"))); // NOI18N
         add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 990, 460));
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnRegistrarRutinaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarRutinaActionPerformed
-        // TODO add your handling code here:
-        /*try {
-            if (rbNuevaRutina.isSelected()) {
-                insertaDatosRutina();
-            } else if (rbActualizar.isSelected()) {
-                ModificarRutina();
-            } else {
-                JOptionPane.showMessageDialog(null, "Debe Selecionar una Accion");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        
-
-        
-        
-        
-    }//GEN-LAST:event_btnRegistrarRutinaActionPerformed
 
     private void RutinasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RutinasMouseClicked
         // TODO add your handling code here:
@@ -111,26 +71,11 @@ public class PanelRutinaCliente extends javax.swing.JPanel {
         
     }//GEN-LAST:event_RutinasMouseClicked
 
-    private void btnselecionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnselecionarActionPerformed
-        // TODO add your handling code here:
-        int filaSeleccionada = Rutinas.getSelectedRow();
-        if (filaSeleccionada != -1) {
-            // Obtener los valores de la fila seleccionada
-            int idClase = (int) Rutinas.getValueAt(filaSeleccionada, 0);
-            lbId.setText(String.valueOf(idClase));
-        } else {
-            // Si no hay fila seleccionada, muestra un mensaje de error
-            JOptionPane.showMessageDialog(null, "Selecciona una fila Para Acutalizar.");
-        }
-    }//GEN-LAST:event_btnselecionarActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Fondo;
     private javax.swing.JTable Rutinas;
     private javax.swing.JScrollPane TablaInstructores;
-    private javax.swing.JButton btnRegistrarRutina;
-    private javax.swing.JButton btnselecionar;
     private javax.swing.JLabel lbId;
     // End of variables declaration//GEN-END:variables
 
